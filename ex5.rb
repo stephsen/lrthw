d71f6cce003d1ff31bc91c16c3b734cec4ea145c
stephsen <stephsen59@gmail.com>
2016-03-06 23:07:40 +0100

    exo suite

--- ex5.rb
+++ ex5.rb
@@ -1,11 +1,18 @@
-mu_name = 'Zed A. Shaw'
-my_age = 35 # not a lie in 2009
-my_height = 74 # inches
-my_weight = 180 # lbs
-my_eyes = 'Blue'
-my_teeth = 'White'
-my_hair = 'Brown'
+name = 'Zed A. Shaw'
+age = 35 # not a lie in 2009
+height = 180 # centimeters
+weight = 90 # Kg
+eyes = 'Blue'
+teeth = 'White'
+hair = 'Brown'
 
 
-puts "Let's talk about #{my_name}."
-puts "He's #{my_height}"
+puts "Let's talk about #{name}."
+puts "He's #{height}"
+puts "He's #{weight} kilo heavy."
+puts "Actually that's not too heavy"
+puts "He's got #{eyes} eyes and #{hair} hair"
+puts "His teeth are usually #{teeth} depending on the coffee"
+
+#This line is tricky, try to get it exactly right
+puts "If I add #{age}, #{height}, #{weight} I get #{age + height + weight}."
